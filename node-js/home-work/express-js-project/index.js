384f19562863e54b07fcdc3ed0446a534e30cc22
Daurim <forlessons.kz@gmail.com>
2024-09-25 17:41:34 +0500

    .

--- node-js/home-work/express-js-project/index.js
+++ node-js/home-work/express-js-project/index.js
@@ -2,15 +2,24 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
 const ejs = require('ejs');
+const session = require('express-session');
 
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(session({
+    secret: 'YouSecretKey',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 8000;
 
-const getProduct = async () => {
+const getProducts = async () => {
     const filePath = path.join(__dirname, 'db', 'products.json');
     const data = await fs.readFile(filePath, 'utf-8');
     return JSON.parse(data);
@@ -21,7 +30,7 @@ const routes = [
     {path: '/product', view: 'products'},
     {path: '/new', view: 'newProduct'},
     {path: '/edit', view: 'editProduct'},
-    {path: '/page', view: 'productPage '},
+    {path: '/cart', view: 'cart'},
 ];
 
 
@@ -35,13 +44,33 @@ routes.forEach(route => {
 
 app.get('/product', async (req, res) => {
     try {
-        const products = await getProduct();
+        const products = await getProducts();
         res.render('products', {products})
     } catch (err) {
         console.error("Error reading the file", err);
         res.status(500).send('Server Error')
     }
+});
+
+app.post('add-to-cart', async(req, res) => {
+    const productId = req.body.productId;
+    const products = await getProducts();
+
+    // Finde product by ID
+    const product = products.find( p => p.id === productId);
+
+    if (!req.session.cart) {
+        req.session.cart = [];
+    }
+
+    req.session.cart.push(product);
+    res.redirect('/product');
 })
+
+app.get('/cart', (req, res) => {
+    const cart = req.session.cart || [];
+    res.render('cart', { cart });
+});
 
 app.listen(PORT, () => {
     console.log(`Port listening on port: ${PORT}`)
