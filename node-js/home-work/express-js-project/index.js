8ec07b180febc41e7b210120bb08d6cc1faa930d
Daurim <greencode.kz@gmail.com>
2024-09-25 08:08:39 +0500

    Created ptoducts list

--- node-js/home-work/express-js-project/index.js
+++ node-js/home-work/express-js-project/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 const ejs = require('ejs');
 
 
@@ -10,8 +10,14 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = 8000;
 
+const getProduct = async () => {
+    const filePath = path.join(__dirname, 'db', 'products.json');
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data);
+}
+
 const routes = [
-    {path: '/', view: 'index', name: 'Home'},
+    {path: '/', view: 'index'},
     {path: '/product', view: 'products'},
     {path: '/new', view: 'newProduct'},
     {path: '/edit', view: 'editProduct'},
@@ -20,11 +26,22 @@ const routes = [
 
 
 routes.forEach(route => {
-    app.get(route.path, (req, res) => {
-        res.render(route.view);
-    });
+    if(route.path !== '/product') {
+        app.get(route.path, (req, res) => {
+            res.render(route.view);
+        });
+    } 
 });
 
+app.get('/product', async (req, res) => {
+    try {
+        const products = await getProduct();
+        res.render('products', {products})
+    } catch (err) {
+        console.error("Error reading the file", err);
+        res.status(500).send('Server Error')
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Port listening on port: ${PORT}`)
