de73df3d9ef36dc60c45523a631664a87590d94f
Daurim <greencode.kz@gmail.com>
2024-10-07 21:27:41 +0500

    add new project

---
+++ next-react/instagram/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import './Footer.css';
+
+export default function Footer() {
+    return(
+        <footer className="footer">
+            <div className="top">
+                <a href="">Meta</a>
+                <a href="">Информация</a>
+                <a href="">Блог</a>
+                <a href="">Вакансии</a>
+                <a href="">Помощь</a>
+                <a href="">API</a>
+                <a href="">Конфиденциальность</a>
+                <a href="">Условия</a>
+                <a href="">Места</a>
+                <a href="">Instagram Lite</a>
+                <a href="">Threads</a>
+                <a href="">Загрузка контактов и лица, не являющиеся пользователями</a>
+                <a href="">Meta Verified</a>
+            </div>
+            <div className="bottom">
+                <div className="language">Language</div>
+                <div>© 2024 Instagram from Meta</div>
+            </div>
+        </footer>
+    )
+}
